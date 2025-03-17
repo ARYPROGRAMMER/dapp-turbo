d8e5ae9284528a815240567020000acf40fefdf5
Arya Pratap Singh <notaryasingh@gmail.com>
2025-03-18 02:05:58 +0530

    clerk ready
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -18,60 +18,59 @@ app.post("/api/v1/website", authMiddleware, async (req, res) => {
 
   res.json({
     id: data.id,
-  })
+  });
 });
-
 
 app.get("/api/v1/website/status", authMiddleware, async (req, res) => {
-    const websiteId = req.query.websiteId! as unknown as string;
-    const userId = req.userId!;
+  const websiteId = req.query.websiteId! as unknown as string;
+  const userId = req.userId!;
 
-    const data = await prismaClient.website.findFirst({
-        where: {
-            id: websiteId,
-            userId
-        },
-        include: {
-            ticks: true
-        }
-    })
+  const data = await prismaClient.website.findFirst({
+    where: {
+      id: websiteId,
+      userId,
+      disabled: false,
+    },
+    include: {
+      ticks: true,
+    },
+  });
 
-    res.json(data);
+  res.json(data);
 });
 
-
 app.get("/api/v1/websites", authMiddleware, async (req, res) => {
-    const userId = req.userId!;
+  const userId = req.userId!;
 
-    const websites = await prismaClient.website.findMany({
-        where: {
-            userId,
-            disabled: false
-        }
-    })
+  const websites = await prismaClient.website.findMany({
+    where: {
+      userId,
+      disabled: false,
+    },
+  });
 
-    res.json({websites})
+  res.json({ websites });
 });
 
 app.delete("/api/v1/website/", authMiddleware, async (req, res) => {
+  const websiteId = req.body.websiteId;
+  const userId = req.userId!;
 
-    const websiteId = req.body.websiteId;
-    const userId = req.userId!;
+  await prismaClient.website.update({
+    where: {
+      id: websiteId,
+      userId,
+    },
+    data: {
+      disabled: true,
+    },
+  });
 
-    await prismaClient.website.update({
-        where: {
-            id: websiteId,
-            userId
-        },
-        data: {
-            disabled: true
-        }
-    })
-
-    res.json({
-        message: "Website deleted successfully"
-    })
-
+  res.json({
+    message: "Website deleted successfully",
+  });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
